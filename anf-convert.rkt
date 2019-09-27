82ae5fd764de786e220d856ed677d3ddd38c23eb
Kris Micinski <krismicinski@administrators-MacBook-Pro.local>
2019-09-27 16:59:40 -0400

    Adding ANF and CPS conversion.

---
+++ anf-convert.rkt
@@ -0,0 +1,68 @@
+#lang racket
+
+(define (fact n)
+  (if (= 0 n)
+      1
+      (* n (fact (- n 1)))))
+
+(define (cps-fact n k)
+  (let ([c (= 0 n)])
+    (if c
+        (k 1)
+        (let ([n-1 (- n 1)])
+          (cps-fact n-1 (lambda (v)
+                          (let ([n*v (* n v)])
+                            (k n*v))))))))
+
+(define (fib n)
+  (if (<= n 1)
+      n
+      (+ (fib (- n 1)) (fib (- n 2)))))
+
+(define (cps-fib n k)
+  (let ([c (<= n 1)])
+    (if c
+        (k n)
+        (let ([n-1 (- n 1)])
+          (cps-fib n-1 (lambda (v0)
+                         (let ([n-2 (- n 2)])
+                           (cps-fib n-2 (lambda (v1)
+                                          (let ([sum (+ v0 v1)])
+                                            (k sum)))))))))))
+
+; Flanagan, et al, 1993 (Essence of compiling with continuations)
+(define (anf-convert e)
+  (define (normalize-ae e k)
+    (normalize e (lambda (anf)
+                   (match anf
+                     [(? symbol? x)
+                      (k x)]
+                     [`(lambda ,xs ,e0)
+                      (k `(lambda ,xs ,e0))]
+                     [else
+                      (define ax (gensym 'a))
+                      `(let ([,ax ,anf])
+                         ,(k ax))]))))
+  (define (normalize-aes es k)
+    (if (null? es)
+        (k '())
+        (normalize-ae (car es) (lambda (ae)
+                                 (normalize-aes (cdr es)
+                                                (lambda (aes)
+                                                  (k `(,ae ,@aes))))))))
+  (define (normalize e k)
+    (match e
+      [(? symbol? x)
+       (k x)]
+      [`(lambda (,x) ,e0)
+       (k `(lambda (,x) ,(anf-convert e0)))]
+      [`(if ,e0 ,e1 ,e2)
+       (normalize-ae e0 (lambda (ae)
+                          (k `(if ,ae ,(anf-convert e1) ,(anf-convert e2)))))]
+      [`(,es ...)
+       #;`(,e0 ,e1)
+       #;(normalize-ae e0 (lambda (ae0)
+                          (normalize-ae e1 (lambda (ae1)
+                                             (k `(,ae0 ,ae1))))))
+       (normalize-aes es k)]))
+  (normalize e (lambda (x) x)))
---
+++ cps-convert.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(define (cps-convert e)
+  (define (T-ae ae)
+    (match ae
+      [(? symbol? x) x]
+      [`(lambda (,x) ,e0)
+       (define k (gensym 'k))
+       `(lambda (,k ,x) ,(T-e e0 k))]
+      [else ae]))
+  (define (T-e e cae)
+    (match e
+      [(? symbol? x)
+       `(,cae ,x ,x)]
+      [`(lambda . ,rest)
+       `(,cae 0 ,(T-ae e))]
+      [`(let ([,x ,e0]) ,e1)
+       (define _x (gensym '_))
+       (T-e e0 `(lambda (,_x ,x) ,(T-e e1 cae)))]
+      [`(if ,ae ,e0 ,e1)
+       `(if ,ae ,(T-e e0 cae) ,(T-e e1 cae))]
+      [`(,aef ,aes ...)
+       `(,(T-ae aef) ,cae ,@(map T-ae aes))]))
+  (T-e e 'halt))
+
+
+(cps-convert '(let ([x ((lambda (z) z) y)]) x))
